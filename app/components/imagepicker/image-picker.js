94c979137672180e66de168bb160f5d48c3c10bd
ganga77 <gs9831@gmail.com>
2024-02-16 23:32:34 -0500

    Adding ImagePicker for share meal component

---
+++ app/components/imagepicker/image-picker.js
@@ -0,0 +1,48 @@
+'use client'
+import { useRef, useState } from 'react'
+import classes from './image-picker.module.css'
+import Image from 'next/image';
+export default function ImagePicker({label, name}){
+    const [pickedImage, setPickedImage] = useState();
+const imageInput = useRef();
+    const handlePickClick = () =>{
+        imageInput.current.click();
+    }
+
+    const handleImageChange = (event)=>{
+        const file = event.target.files[0];
+
+        if(!file){
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.onload = () =>{
+            setPickedImage(fileReader.result);
+        }
+        fileReader.readAsDataURL(file);
+    }
+
+    return (
+        <div className={classes.picker}>
+            <label htmlFor={name}>{label}</label>
+            <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No Image Picked Yet!</p>}
+                    {pickedImage && <Image src={pickedImage} 
+                     alt="The selected image" 
+                     fill/>}
+                </div>
+                <input
+                className={classes.input}
+                type="file"
+                id={name}
+                accept="image/png, image/jpeg"
+                name={name}
+                ref={imageInput} 
+                onChange={handleImageChange}/> 
+                <button className={classes.button} type="button" onClick={handlePickClick}>Pick an image</button>
+            </div>
+        </div>
+    )
+}
